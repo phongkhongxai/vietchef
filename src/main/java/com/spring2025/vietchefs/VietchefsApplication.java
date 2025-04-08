a9757f3a37bae8ff72fa0232041b231d634db557
phongkoxai <thieuphong2003@gmail.com>
2025-04-08 15:38:48 +0700

    hehe12

--- src/main/java/com/spring2025/vietchefs/VietchefsApplication.java
+++ src/main/java/com/spring2025/vietchefs/VietchefsApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @EnableAsync
 @SpringBootApplication
 public class VietchefsApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh")); // Ép timezone JVM
 		SpringApplication.run(VietchefsApplication.class, args);
 	}
 
