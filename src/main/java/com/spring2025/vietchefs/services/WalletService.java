535414eb5d0eedd2ca482bfc3007537bfedf3d01
phongkoxai <thieuphong2003@gmail.com>
2025-04-08 16:33:47 +0700

    add wallet

--- src/main/java/com/spring2025/vietchefs/services/WalletService.java
+++ src/main/java/com/spring2025/vietchefs/services/WalletService.java
@@ -1,6 +1,9 @@
 package com.spring2025.vietchefs.services;
 
+import com.spring2025.vietchefs.models.payload.dto.WalletDto;
+
 public interface WalletService {
     void createWallet(Long userId, String walletType);
     void updateWalletType(Long userId, String newType);
+    WalletDto getWalletByUserId(Long userId);
 }
