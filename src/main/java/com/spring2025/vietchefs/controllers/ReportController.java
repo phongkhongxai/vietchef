6005dcd8e80bb7b7f716570f910263330a3b301e
phongkoxai <thieuphong2003@gmail.com>
2025-05-05 18:04:23 +0700

    ft: report process for admin"

--- src/main/java/com/spring2025/vietchefs/controllers/ReportController.java
+++ src/main/java/com/spring2025/vietchefs/controllers/ReportController.java
@@ -3,6 +3,7 @@ package com.spring2025.vietchefs.controllers;
 import com.spring2025.vietchefs.models.entity.Report;
 import com.spring2025.vietchefs.models.payload.dto.ReportDto;
 import com.spring2025.vietchefs.models.payload.dto.UserDto;
+import com.spring2025.vietchefs.models.payload.requestModel.ReportHandleRequest;
 import com.spring2025.vietchefs.models.payload.requestModel.ReportRequest;
 import com.spring2025.vietchefs.models.payload.responseModel.ReportsResponse;
 import com.spring2025.vietchefs.services.ReportService;
@@ -28,7 +29,7 @@ public class ReportController {
     @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     @PostMapping
     @Operation(
-            summary = "Tạo report với reason là CHEF_NO_SHOW "
+            summary = "Tạo report với reason là CHEF_NO_SHOW và phải có bookingDetailId, ko cần reviewId "
     )
     public ResponseEntity<ReportDto> createChefNoShowReport(
             @AuthenticationPrincipal UserDetails userDetails,
@@ -36,6 +37,20 @@ public class ReportController {
     ) {
         UserDto bto = userService.getProfileUserByUsernameOrEmail(userDetails.getUsername(),userDetails.getUsername());
         ReportDto createdReport = reportService.createReportWithChefNoShow(bto.getId(), request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdReport);
+    }
+    @SecurityRequirement(name = "Bearer Authentication")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
+    @PostMapping("/others")
+    @Operation(
+            summary = "Tạo report với reason là khác và đi kèm ReviewId, ko cần bookingDetailId "
+    )
+    public ResponseEntity<ReportDto> createReportWithReview(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody ReportRequest request
+    ) {
+        UserDto bto = userService.getProfileUserByUsernameOrEmail(userDetails.getUsername(),userDetails.getUsername());
+        ReportDto createdReport = reportService.createReportWithOtherReason(bto.getId(), request);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdReport);
     }
     @SecurityRequirement(name = "Bearer Authentication")
@@ -49,6 +64,16 @@ public class ReportController {
         return ResponseEntity.ok(reportService.getAllReports(pageNo, pageSize, sortBy, sortDir));
     }
     @SecurityRequirement(name = "Bearer Authentication")
+    @GetMapping("/pending")
+    public ResponseEntity<ReportsResponse> getAllPendingReports(
+            @RequestParam(value = "pageNo", defaultValue = "0") int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "createdAt") String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "desc") String sortDir
+    ) {
+        return ResponseEntity.ok(reportService.getAllReportsPending(pageNo, pageSize, sortBy, sortDir));
+    }
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("/{id}")
     public ResponseEntity<ReportDto> getReportById(@PathVariable Long id) {
         return ResponseEntity.ok(reportService.getReportById(id));
@@ -57,8 +82,8 @@ public class ReportController {
     @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}/status")
-    public ResponseEntity<ReportDto> updateReportStatus(@PathVariable Long id, @RequestParam String status) {
-        return ResponseEntity.ok(reportService.updateReportStatus(id, status));
+    public ResponseEntity<ReportDto> updateReportStatus(@PathVariable Long id, @RequestBody ReportHandleRequest request) {
+        return ResponseEntity.ok(reportService.updateReportStatus(id, request));
     }
     @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
