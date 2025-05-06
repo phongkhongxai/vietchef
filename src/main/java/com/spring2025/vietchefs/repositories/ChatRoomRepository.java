7da13b6eda8f1481d72a1821acf946591c2b7016
phongkoxai <thieuphong2003@gmail.com>
2025-05-06 08:53:03 +0700

    fix: get chat room

--- src/main/java/com/spring2025/vietchefs/repositories/ChatRoomRepository.java
+++ src/main/java/com/spring2025/vietchefs/repositories/ChatRoomRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+    Optional<ChatRoom> findByChatId(String chatId);
+
 }
