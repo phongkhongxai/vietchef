3d64432bdfc102bb6d0d90527e8270989415f42d
phongkoxai <thieuphong2003@gmail.com>
2025-05-19 16:15:10 +0700

    add docs for chatbox2

--- src/main/java/com/spring2025/vietchefs/controllers/ChatbotController.java
+++ src/main/java/com/spring2025/vietchefs/controllers/ChatbotController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/chatbox")
+@RequestMapping("/api/v1/chatbot")
 public class ChatbotController {
 
     private final ChatbotService chatbotService;
